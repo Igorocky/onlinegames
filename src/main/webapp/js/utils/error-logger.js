b4a55e0cd8f520d97d9285a386b22b305483e275
Igorocky <Igorocky@users.noreply.github.com>
2020-08-22 08:26:55 +0200

    Add APP_VERSION to FE error logger.

--- src/main/webapp/js/utils/error-logger.js
+++ src/main/webapp/js/utils/error-logger.js
@@ -4,7 +4,7 @@ window.onerror = function (message, file, line, col, error) {
     $.ajax({
         type: "POST",
         url: "/be/log-fe-error",
-        data: JSON.stringify({error, message, file, line, col}),
+        data: JSON.stringify({error, message, file, line, col, appVersion: APP_VERSION}),
         contentType: "application/json; charset=utf-8"
     });
     return true;
